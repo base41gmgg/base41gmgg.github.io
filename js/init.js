74ea293f37c570321d9bfdc2af262ae6cf266868
formulariogmgg <formulariogmgg@gmail.com>
2023-03-23 22:22:28 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCWFIzaIyV8C5_HuN2av1gLwI--2MmQvk8",
+    authDomain: "base41gmgg1.firebaseapp.com",
+    projectId: "base41gmgg1",
+    storageBucket: "base41gmgg1.appspot.com",
+    messagingSenderId: "452065519790",
+    appId: "1:452065519790:web:db7ed090c2f2555dfbe53f",
+    measurementId: "G-W034X5FPEY"
+});
